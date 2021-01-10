ff265938882d14e4b81386d708b02f51f0e1fcc2
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 13:33:09 +0100

    träning om sort i Array method

---
+++ main.js
@@ -0,0 +1,17 @@
+/* global console, alert, prompt */
+
+var friends = [
+"Mah",
+"Ali",
+"Anton",
+"Soha",
+"Batrik",
+"Samer",
+"Nael"
+
+];
+console.log(friends);
+
+friends.sort();
+
+console.log();
